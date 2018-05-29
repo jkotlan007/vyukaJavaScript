1bbb2b58ef17e07c73d5a100fe3565d61008977a
Jaroslav Kotlán <jkotlan@seznam.cz>
2018-05-29 12:47:14 +0200

    Lekce 11 - graphql id type

--- graphql-playlist/server/schema/schema.js
+++ graphql-playlist/server/schema/schema.js
@@ -1,7 +1,12 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID
+} = graphql;
 
 let books = [
     {name:'Saturnin', genre:'Komedie', id:'1'},
@@ -12,7 +17,7 @@ let books = [
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:() => ({
-        id:{type:GraphQLString},
+        id:{type:GraphQLID},
         name: {type:GraphQLString},
         genre:{type:GraphQLString}
     })
@@ -23,9 +28,10 @@ const RootQuery = new GraphQLObjectType({
     fields:{
         book:{
             type: BookType,
-            args:{id:{type: GraphQLString}},
+            args:{id:{type: GraphQLID}},
             resolve(parent,args){
                 //získání dat z databáze neno odjinud
+                console.log(typeof(args.id));
             return _.find(books, {id: args.id})
             }
         }
