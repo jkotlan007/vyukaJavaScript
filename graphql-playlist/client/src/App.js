97bcfac888e468ce6a69c61c57967c1a6f7aea96
Jaroslav Kotlán <jkotlan@seznam.cz>
2018-05-31 11:09:53 +0200

    Lekce 24 - book list component

--- graphql-playlist/client/src/App.js
+++ graphql-playlist/client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+//components
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>Jardův seznam knih</h1>
+        <BookList/>
       </div>
     );
   }
